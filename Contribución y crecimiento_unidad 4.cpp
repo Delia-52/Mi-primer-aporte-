8e14ec8afdbca353d0e775388ff14620fc3091d7
Delia-52 <delyprincess0230@gmail.com>
2024-12-09 16:00:07 -0600

    Add files via upload

---
+++ Contribución y crecimiento_unidad 4.cpp
@@ -0,0 +1,42 @@
+/*UNIVERSIDAD ROSARIO CASTELLANOS"
+SISTEMAS DE DISTRIBUCIÓN ABIERTA
+UNIDAD 4. VALORACIONES ORGANIZACIONALES
+ALUMNA: ROMERO LOPEZ DELIA
+GRUPO: G-801
+DOCENTE; DAN MARRÓN MUÑOZ
+*/
+
+#include <iostream>
+using namespace std;
+
+void flujoColaboracion() {
+    cout << "Identificación de la Necesidad" << endl;
+    cout << "?" << endl;
+    cout << "Búsqueda de Software Open Source" << endl;
+    cout << "?" << endl;
+    cout << "Selección del Software Apropiado" << endl;
+    cout << "?" << endl;
+    cout << "Formación del Equipo de Trabajo" << endl;
+    cout << "?" << endl;
+    cout << "Definición de Objetivos y Alcance" << endl;
+    cout << "?" << endl;
+    cout << "Desarrollo del Proyecto" << endl;
+    cout << "?" << endl;
+    cout << "Desarrollo de Funcionalidades Básicas" << endl;
+    cout << "?" << endl;
+    cout << "Integración de Servicios" << endl;
+    cout << "?" << endl;
+    cout << "Desarrollo de Funcionalidades Avanzadas" << endl;
+    cout << "?" << endl;
+    cout << "Pruebas y Revisión" << endl;
+    cout << "?" << endl;
+    cout << "Implementación" << endl;
+    cout << "?" << endl;
+    cout << "Mantenimiento y Mejora Continua" << endl;
+}
+
+int main() {
+    flujoColaboracion();
+    return 0;
+}
+
